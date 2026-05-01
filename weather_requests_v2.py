2a4ae1c9bafffc5ddb62bbc1f7071d6bfbf33dd0
Naipan Abdinegara <naipan.abdinegara@outlook.co.id>
2026-05-02 00:32:21 +0700

    Add files via upload

---
+++ weather_requests_v2.py
@@ -0,0 +1,79 @@
+import requests, datetime, os
+
+print("-" * 30)
+print("Learning Weather requests with OpenMeteo API")
+print("-" * 30)
+
+date = datetime.datetime.now().strftime("%Y-%m-%d")
+get_information = ["temperature_2m", "wind_speed_10m", "shortwave_radiation", "diffuse_radiation", "visibility", "weathercode" ]
+
+def chk_geocode(location): 
+    BASE_URL = "https://nominatim.openstreetmap.org/search"
+    params = {
+        "q": location.lower(),
+        "format": "json"
+    }
+
+    headers = {
+        "User-Agent": "LearningWeatherApp/1.0 (naipan.abdinegara@outlook.co.id)"
+    }
+
+    data = requests.get(url=BASE_URL, headers=headers, params=params, timeout=10).json()
+    return data
+
+def chk_forecast(latitude, longitude):
+    BASE_URL = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "current": ",".join(get_information)
+    }
+    data = requests.get(url=BASE_URL, params=params, timeout=10).json()
+    return data
+
+while True:
+    print("Enter your location name")
+    try:
+        location = str(input("> "))
+        geocode_data = chk_geocode(location=location)
+        latitude = geocode_data[0]["lat"]
+        longitude = geocode_data[0]["lon"]
+
+        print("-" * 20)
+        print(f"Fetched latitude: {latitude}")
+        print(f"Fetched longitude: {longitude}")
+        print("-" * 20)
+        forecast_data = chk_forecast(latitude=latitude, longitude=longitude)
+        current_forecast = forecast_data["current"]
+        temperature = current_forecast["temperature_2m"]
+        wind_speed = current_forecast["wind_speed_10m"]
+        shortwave_radiation = current_forecast["shortwave_radiation"]
+        weathercode = current_forecast["weathercode"]
+        print("Forecast Information: ")
+        print(f"Temperature: {temperature}")
+        print(f"Wind Speed: {wind_speed}")
+        print(f"ShortWave Radiation: {shortwave_radiation}")
+        print(f"Weather Code: {weathercode}")
+        print("-" * 20)
+
+        with open("weather.csv", "a") as file:
+            csv_exists = os.path.isfile("weather.csv")
+            if not csv_exists:
+                file.write("date,location,temp,wind,radiation,weathercode")
+            file.write(f"\n{date},{location},{temperature},{wind_speed},{shortwave_radiation},{weathercode}")
+
+    except requests.Timeout:
+        print("Connection Timeout")
+        break
+    except AttributeError:
+        print("The structured changed, check the code again.")
+    except KeyboardInterrupt:
+        print("\nProgram stopped.")
+        break
+    except OSError:
+        print("Unable to write an output to weather.csv")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occured: {e}")
+    except Exception as e:
+        print("The program didn't work properly!")
+        print(f"Error: {e}")
